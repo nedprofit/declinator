883f55a2549aa0a14283c14f5997c37ebf9a12d4
Nouara <nedprofit@gmail.com>
2022-03-05 14:04:54 +0300

    add bootstrap

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem "rails", "~> 7.0.2", ">= 7.0.2.2"
 gem 'petrovich'
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
-
+gem 'cssbundling-rails'
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   root 'data#new'
   post '/', to: 'data#create'
   get 'new', to: 'data#new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
